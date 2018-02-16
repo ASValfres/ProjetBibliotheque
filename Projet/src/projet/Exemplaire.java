1a41d103ca02847fddf874281fa2d2beca9ba423
guevarag <guevarag@pc-dg-025-12.iut2.upmf-grenoble.fr>
2018-02-16 14:11:33 +0100

    Merge origin/master into HEAD

--- Projet/src/projet/Exemplaire.java
+++ Projet/src/projet/Exemplaire.java
@@ -5,10 +5,45 @@
  */
 package projet;
 
+import java.util.Date;
+
 /**
  *
  * @author valfres
  */
 public class Exemplaire {
-    bonjour
+    private Boolean _empruntable;
+    private int _numeroexemplaire;
+    private Date _datereception;
+    
+    public Exemplaire(Ouvrage ouvrage, Boolean empruntable,Date dateReception, int numeroExemplaire){
+        this._empruntable=empruntable;
+        this._numeroexemplaire=numeroExemplaire;
+        this._datereception=dateReception;
+        lierExemplaireOuvrage(ouvrage);
+    }
+    
+    public Exemplaire getExemplaire(int ISBN, int numeroExemplaire){
+        Exemplaire exemplaire=new Exemplaire();
+        exemplaire=getExemplaire(ISBN, numeroExemplaire);
+        return exemplaire;
+    }
+    
+    public void afficherInfos(int ISBN, int numeroExemplaire){
+        Exemplaire exemplaire=new Exemplaire();
+        exemplaire=getExemplaire(ISBN, numeroExemplaire);
+        
+        System.out.println("\nNuméro exemplaire :"+exemplaire._numeroexemplaire);
+        if(exemplaire._empruntable){
+            System.out.println("Empruntable");
+        }
+        else{
+            System.out.println("Non empruntable");
+        }
+        System.out.println("Date reception : "+exemplaire._datereception);
+           
+    }
+    
+    public void afficherExem
+    
 }
